721cf128f1563e9a16bb3dba08f850b569cf5873
Gillett Hernandez <gillett.hernandez@gmail.com>
2016-04-07 16:40:45 -0700

    rm todo.txt and added issues to tracker

--- loggable_dec.py
+++ loggable_dec.py
@@ -1,5 +1,8 @@
 import logging
 
+logging.basicConfig(filename="hello.log", filemode="w", level=logging.DEBUG)
+
+
 def loggable(f):
     def func(*args, **kwargs):
         logging.debug(f.__name__+", args="+str(args)+", kwargs="+str(kwargs))
@@ -8,3 +11,17 @@ def loggable(f):
         return result
     func.__name__ = f.__name__
     return func
+
+
+@loggable
+def butts(x, y):
+    return x + y
+
+
+def buggs(x, y):
+    return x + y
+
+buggs = loggable(buggs)
+
+print(butts(4, 5))
+print(buggs(4, 5))
